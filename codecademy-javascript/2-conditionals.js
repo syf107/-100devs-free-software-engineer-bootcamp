c10b8ed61f96bb9434fcd66df2481b4f30108f0a
syf107 <syariefarifin107@gmail.com>
2022-01-02 15:06:54 +0700

    finished the conditionals section

--- codecademy-javascript/2-conditionals.js
+++ codecademy-javascript/2-conditionals.js
@@ -3,33 +3,33 @@ let sale = true;
 sale = false;
 
 if (sale) {
-  console.log('Time to buy!')
+  console.log("Time to buy!");
 }
 
 // If...Else Statements
-if(sale) {
-  console.log('Time to buy!');
+if (sale) {
+  console.log("Time to buy!");
 } else {
-  console.log('Time to wait for a sale.')
+  console.log("Time to wait for a sale.");
 }
 
 // Comparison Operators
 let hungerLevel = 7;
 
 if (hungerLevel > 7) {
-  console.log('Time to eat!');
+  console.log("Time to eat!");
 } else {
-  console.log('We can eat later!');
+  console.log("We can eat later!");
 }
 
 // Logical Operators
-let mood = 'sleepy';
+let mood = "sleepy";
 let tirednessLevel = 6;
 
-if (mood === 'sleepy' && tirednessLevel > 8) {
-  console.log('time to sleep')
+if (mood === "sleepy" && tirednessLevel > 8) {
+  console.log("time to sleep");
 } else {
-  console.log('not bed time yet')
+  console.log("not bed time yet");
 }
 
 // Truthy and Falsy
@@ -38,70 +38,148 @@ let wordCount = 5;
 if (wordCount) {
   console.log("Great! You've started your work!");
 } else {
-  console.log('Better get to work!');
+  console.log("Better get to work!");
 }
 
-
-let favoritePhrase = '';
+let favoritePhrase = "";
 
 if (favoritePhrase) {
   console.log("This string doesn't seem to be empty.");
 } else {
-  console.log('This string is definitely empty.');
+  console.log("This string is definitely empty.");
 }
 
-
 // Truthy and Falsy Assignment
-let tool = 'marker';
+let tool = "marker";
 
 // Use short circuit evaluation to assign  writingUtensil variable below:
-let writingUtensil = tool || 'pen'
+let writingUtensil = tool || "pen";
 
 console.log(`The ${writingUtensil} is mightier than the sword.`);
 
 // Ternary Operator
 let isLocked = false;
 
-isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
-
+isLocked
+  ? console.log("You will need a key to open the door.")
+  : console.log("You will not need a key to open the door.");
 
 let isCorrect = true;
-isCorrect ?   console.log('Correct!') :   console.log('Incorrect!');
+isCorrect ? console.log("Correct!") : console.log("Incorrect!");
 
+let favoritePhrase = "Love That!";
 
-let favoritePhrase = 'Love That!';
-
-favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+favoritePhrase === "Love That!"
+  ? console.log("I love that!")
+  : console.log("I don't love that!");
 
 // Else If Statements
-let season = 'summer';
+let season = "summer";
 
-if (season === 'spring') {
-  console.log('It\'s spring! The trees are budding!');
-} else if (season === 'winter') {
-  console.log('It\'s winter! Everything is covered in snow.')
-} else if (season === 'fall') {
-  console.log('It\'s fall! Leaves are falling!');
-} else if (season === 'summer') {
-  console.log('It\'s sunny and warm because it\'s summer!');
+if (season === "spring") {
+  console.log("It's spring! The trees are budding!");
+} else if (season === "winter") {
+  console.log("It's winter! Everything is covered in snow.");
+} else if (season === "fall") {
+  console.log("It's fall! Leaves are falling!");
+} else if (season === "summer") {
+  console.log("It's sunny and warm because it's summer!");
 } else {
-  console.log('Invalid season.');
+  console.log("Invalid season.");
 }
 
 // The switch keyword
-let athleteFinalPosition = 'first place';
+let athleteFinalPosition = "first place";
 
 switch (athleteFinalPosition) {
-  case 'first place':
-    console.log('You get the gold medal!');
+  case "first place":
+    console.log("You get the gold medal!");
     break;
-  case 'second place': 
-    console.log('You get the silver medal!');
+  case "second place":
+    console.log("You get the silver medal!");
     break;
-  case 'third place':
-    console.log('You get the bronze medal!');
+  case "third place":
+    console.log("You get the bronze medal!");
     break;
-  default: 
-    console.log('No medal awarded.');
+  default:
+    console.log("No medal awarded.");
     break;
+}
+
+// Magic Eight Ball
+let userName = "Syarief";
+
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+
+let userQuestion = "Do you miss me?";
+console.log(userQuestion, userName);
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My source say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+console.log(eightBall);
+
+if (randomNumber === 0) {
+  eightBall = "It is certain";
+} else if (randomNumber === 1) {
+  eightBall = "It is decidedly so";
+} else if (randomNumber === 2) {
+  eightBall = "Reply hazy try again";
+} else if (randomNumber === 3) {
+  eightBall = "Cannot predict now";
+} else if (randomNumber === 4) {
+  eightBall = "Do not count on it";
+} else if (randomNumber === 5) {
+  eightBall = "My source say no";
+} else if (randomNumber === 6) {
+  eightBall = "Outlook not so good";
+} else if (randomNumber === 7) {
+  eightBall = "Signs point to yes";
+}
+
+console.log(eightBall);
+
+// Race Day
+let raceNumber = Math.floor(Math.random() * 1000);
+let earlyRegistered = false;
+let runnerAge = 18;
+
+if (earlyRegistered && runnerAge > 18) {
+  raceNumber += 1000;
+}
+
+if (runnerAge > 18 && earlyRegistered) {
+  console.log(`You will race at 9:30 am, ${raceNumber}.`);
+} else if (runnerAge > 18 && !earlyRegistered) {
+  console.log(`You will race at 11:00 am, ${raceNumber}.`);
+} else if (runnerAge < 18) {
+  console.log(`You will race at 12:30pm, ${raceNumber}.`);
+} else {
+  console.log("You have to see the registration deck.");
 }

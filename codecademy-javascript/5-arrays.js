ca81f81d29bb80da35c48b8273741cc0148ff446
syf107 <syariefarifin107@gmail.com>
2022-01-03 22:56:53 +0700

    adding scope and arrays, and finished to section 6, time for another section

---
+++ codecademy-javascript/5-arrays.js
@@ -0,0 +1,102 @@
+// Create an array
+const hobbies = ["Coding", "Watching movies", "Riding motorcycle"];
+console.log(hobbies);
+
+// Accessing Elements
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life.",
+];
+
+let listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
+
+// Update Elements
+let groceryList = ["bread", "tomatoes", "milk"];
+
+groceryList[1] = "avocados";
+
+// Arrays with let and const
+let condiments = ["Ketchup", "Mustard", "Soy Sauce", "Sriracha"];
+
+const utensils = ["Fork", "Knife", "Chopsticks", "Spork"];
+
+condiments[0] = "Mayo";
+console.log(condiments);
+condiments = ["Mayo"];
+console.log(condiments);
+utensils[3] = "Spoon";
+console.log(utensils);
+
+// The .length property
+const objectives = ["Learn a new languages", "Read 52 books", "Run a marathon"];
+
+console.log(objectives.length);
+
+// The .push() Method
+const chores = ["wash dishes", "do laundry", "take out trash"];
+
+chores.push("take a bath", "go to sleep");
+console.log(chores);
+
+// The .pop() Method
+const chores = [
+  "wash dishes",
+  "do laundry",
+  "take out trash",
+  "cook dinner",
+  "mop floor",
+];
+
+chores.pop();
+console.log(chores);
+
+// More Array Methods
+const groceryList = [
+  "orange juice",
+  "bananas",
+  "coffee beans",
+  "brown rice",
+  "pasta",
+  "coconut oil",
+  "plantains",
+];
+
+groceryList.shift();
+
+groceryList.unshift("popcorn");
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf("pasta");
+console.log(pastaIndex);
+
+// Arrays and Functions
+const concept = ["arrays", "can", "be", "mutated"];
+
+function changeArr(arr) {
+  arr[3] = "MUTATED";
+}
+
+changeArr(concept);
+
+console.log(concept);
+function removeElement(newArr) {
+  newArr.pop();
+}
+
+removeElement(concept);
+console.log(concept);
+
+// Nested Array
+const numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const target = numberClusters[2][1];
+console.log(target);

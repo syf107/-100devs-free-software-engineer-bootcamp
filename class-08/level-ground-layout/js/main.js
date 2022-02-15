b0d4f1253c217c0c60d867fbdcc1fc750bb93592
syf107 <syariefarifin107@gmail.com>
2022-02-15 23:55:42 +0700

    make the responsiveness for level ground layout.

--- class-08/level-ground-layout/js/main.js
+++ class-08/level-ground-layout/js/main.js
@@ -1,1 +1,14 @@
+const headerEl = document.querySelector(".header");
+const navBtn = document.querySelector(".btn-mobile-nav");
 
+navBtn.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allNavLink = document.querySelectorAll(".nav-link");
+
+allNavLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    headerEl.classList.remove("nav-open");
+  });
+});

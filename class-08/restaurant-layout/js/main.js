0d6c7f0b231255ba08cb8c8a576229df2f55054f
syf107 <syariefarifin107@gmail.com>
2022-03-16 00:27:51 +0700

    adding another thing for restaurant layout.

--- class-08/restaurant-layout/js/main.js
+++ class-08/restaurant-layout/js/main.js
@@ -1,1 +1,57 @@
+// adding sticky header follow with background.
+const sectionHeroEl = document.querySelector(".hero-header");
 
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// adding button to make food picture appear based on its category
+
+const allCategory = document.querySelector(".all-category");
+const breakfastCategory = document.querySelector(".breakfast-category");
+const lunchCategory = document.querySelector(".lunch-category");
+const dinnerCategory = document.querySelector(".dinner-category");
+const dessertCategory = document.querySelector(".desserts-category");
+const shakeCategory = document.querySelector(".shake-category");
+const foodPicAll = document.querySelectorAll(".food-pic");
+
+allCategory.addEventListener("click", showAll);
+breakfastCategory.addEventListener("click", () => onlyCategory("breakfast"));
+lunchCategory.addEventListener("click", () => onlyCategory("lunch"));
+dinnerCategory.addEventListener("click", () => onlyCategory("dinner"));
+dessertCategory.addEventListener("click", () => onlyCategory("desserts"));
+shakeCategory.addEventListener("click", () => onlyCategory("shake"));
+
+function onlyCategory(category) {
+  foodPicAll.forEach((foodPic) => {
+    if (!foodPic.classList.contains(category)) {
+      foodPic.style.display = "none";
+    } else {
+      foodPic.style.display = "block";
+    }
+  });
+}
+
+function showAll() {
+  foodPicAll.forEach((foodPic) => {
+    foodPic.style.display = "block";
+  });
+}
